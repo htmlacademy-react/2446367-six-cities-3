22e9b534055edf05692be6dccf31e8d10b95e147
Sasha Kandaurow <al.kandaurow@yandex.ru>
2024-12-17 01:44:18 +0700

    add css for not-found-page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,7 +1,15 @@
-export default function NotFoundPage () {
+import './not-found-page.css';
+import { Link } from 'react-router-dom';
+
+export default function NotFoundPage() {
   return (
     <main className="page__main page__not-found">
-      404 Not Found
+      <div className="not-found">
+        <p className="not-found__descr">404. Not Found</p>
+      </div>
+      <p className="not-found__link">
+        <Link to="/">Вернуться на главную</Link>
+      </p>
     </main>
   );
 }
