7827e92ff8ec0f027dc4e785b87f018d50722ac9
Sasha Kandaurow <al.kandaurow@yandex.ru>
2025-02-07 14:20:57 +0700

    refactor reducer

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,37 +1,22 @@
 import { Offers } from '../mocks/mock-types/offers';
 import { CITIES, CityName } from '../utils/data';
 import { mockOffers } from '../mocks/mock-offers';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
-interface OffersState {
+type OffersState = {
   city: CityName;
   mockOffers: Offers;
-}
+};
 
 const initialState: OffersState = {
   city: CITIES[0].name,
   mockOffers,
 };
 
-const enum ActionType {
-  SetCity = 'offers/setCity',
-}
+export const setCity = createAction<CityName>('offers/setCity');
 
-export const setCity = (city: CityName) => ({
-  payload: city,
-  type: ActionType.SetCity,
+export const reducer = createReducer(initialState, (builder) => {
+  builder.addCase(setCity, (state, action) => {
+    state.city = action.payload;
+  });
 });
-
-export function reducer(
-  state: OffersState = initialState,
-  action: { payload: unknown; type: ActionType },
-): OffersState {
-  switch (action.type) {
-    case ActionType.SetCity:
-      return {
-        ...state,
-        city: action.payload as CityName,
-      };
-    default:
-      return state;
-  }
-}
