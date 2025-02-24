c4f209560eda2ddc26136ce3462a5b138031acd2
Sasha Kandaurow <al.kandaurow@yandex.ru>
2025-02-24 19:37:01 +0700

    change project structure

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,24 +1,24 @@
 import { useEffect } from 'react';
-import { useActionCreators, useAppSelector } from './hooks/store';
+import { useActionCreators, useAppSelector } from '../../hooks/store';
 
 import { ToastContainer } from 'react-toastify';
 
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import FavoritesPage from './pages/favorites-page/favorites-page';
-import LoginPage from './pages/login-page/login-page';
-import MainPage from './pages/main-page/main-page';
-import OfferPage from './pages/offer-page/offer-page';
-import NotFoundPage from './pages/not-found-page/not-found-page';
-import ProtectedRoute from './components/private-route/private-route';
-import OffersProvider from './contexts/offers-context';
-import Layout from './components/layout/layout';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import LoginPage from '../../pages/login-page/login-page';
+import MainPage from '../../pages/main-page/main-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import ProtectedRoute from '../private-route/private-route';
+import OffersProvider from '../../contexts/offers-context';
+import Layout from '../layout/layout';
 
-import { AppRoute } from './utils/data/data';
-import { userActions } from './store/slices/user/user';
-import { offersActions } from './store/slices/offers/offers';
-import { selectCityOffers } from './store/selectors/offers';
+import { AppRoute } from '../../utils/data/data';
+import { userActions } from '../../store/slices/user/user';
+import { offersActions } from '../../store/slices/offers/offers';
+import { selectCityOffers } from '../../store/selectors/offers';
 
-import { getToken } from './services/token';
+import { getToken } from '../../services/token';
 
 export default function App() {
   const { fetchAllOffers } = useActionCreators(offersActions);
